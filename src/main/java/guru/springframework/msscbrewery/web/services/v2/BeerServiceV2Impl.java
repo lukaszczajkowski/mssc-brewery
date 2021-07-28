c9c825dc6c3c5dd320e1f822faaea6e7d20a2e68
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2021-07-28 09:55:50 +0200

    V2 implemented

---
+++ src/main/java/guru/springframework/msscbrewery/web/services/v2/BeerServiceV2Impl.java
@@ -0,0 +1,38 @@
+package guru.springframework.msscbrewery.web.services.v2;
+
+import guru.springframework.msscbrewery.web.model.v2.BeerDtoV2;
+import guru.springframework.msscbrewery.web.model.v2.BeerStyleEnum;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class BeerServiceV2Impl implements BeerServiceV2 {
+    @Override
+    public BeerDtoV2 getBeerById(UUID beerId) {
+        return BeerDtoV2.builder()
+                .id(beerId)
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyleEnum.ALE)
+                .build();
+    }
+
+    @Override
+    public BeerDtoV2 saveNewBeer(BeerDtoV2 beerDto) {
+        return BeerDtoV2.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDtoV2 beerDto) {
+        //todo impl - would add a real impl to update beer
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.debug("Deleting a beer...");
+    }
+}
